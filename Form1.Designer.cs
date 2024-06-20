df8fe746cf2f215a722718367f583c9696c811e4
BIKKO <njnjirf238@gmail.com>
2024-06-20 15:31:58 +0700

    Переделанно ядро сервера с EasyModBus на NModBus Реализованно запрос-ответное общение

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,56 +29,108 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            progressBar1 = new ProgressBar();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            listlog = new ListBox();
+            label1 = new Label();
             review = new Button();
             Location = new TextBox();
             StartStop = new Button();
             panel1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
-            panel1.Controls.Add(progressBar1);
-            panel1.Controls.Add(review);
-            panel1.Controls.Add(Location);
-            panel1.Controls.Add(StartStop);
+            panel1.Controls.Add(tabControl1);
             panel1.Dock = DockStyle.Fill;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(554, 288);
+            panel1.Size = new Size(390, 492);
             panel1.TabIndex = 0;
             // 
-            // progressBar1
+            // tabControl1
             // 
-            progressBar1.Location = new Point(68, 238);
-            progressBar1.Name = "progressBar1";
-            progressBar1.Size = new Size(396, 15);
-            progressBar1.TabIndex = 3;
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(390, 492);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(listlog);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(review);
+            tabPage1.Controls.Add(Location);
+            tabPage1.Controls.Add(StartStop);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(382, 459);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(0, 0);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listlog
+            // 
+            listlog.Dock = DockStyle.Bottom;
+            listlog.FormattingEnabled = true;
+            listlog.ItemHeight = 20;
+            listlog.Location = new Point(3, 172);
+            listlog.Name = "listlog";
+            listlog.Size = new Size(376, 284);
+            listlog.TabIndex = 20;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(146, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(81, 35);
+            label1.TabIndex = 19;
+            label1.Text = "label1";
             // 
             // review
             // 
-            review.Location = new Point(419, 181);
+            review.Location = new Point(289, 124);
             review.Name = "review";
             review.Size = new Size(94, 29);
-            review.TabIndex = 2;
+            review.TabIndex = 18;
             review.Text = "Обзор";
             review.UseVisualStyleBackColor = true;
             review.Click += review_Click;
             // 
             // Location
             // 
-            Location.Location = new Point(68, 183);
+            Location.Location = new Point(8, 124);
             Location.Name = "Location";
-            Location.Size = new Size(345, 27);
-            Location.TabIndex = 1;
+            Location.Size = new Size(275, 27);
+            Location.TabIndex = 17;
             // 
             // StartStop
             // 
             StartStop.BackColor = Color.Green;
-            StartStop.Location = new Point(68, 33);
+            StartStop.Location = new Point(75, 48);
             StartStop.Name = "StartStop";
-            StartStop.Size = new Size(445, 118);
-            StartStop.TabIndex = 0;
+            StartStop.Size = new Size(218, 70);
+            StartStop.TabIndex = 16;
             StartStop.Text = "Старт";
             StartStop.UseVisualStyleBackColor = false;
             StartStop.Click += StartStop_Click;
@@ -87,21 +139,29 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(554, 288);
+            ClientSize = new Size(390, 492);
             Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
             Name = "Form1";
-            Text = "Form1";
+            ShowIcon = false;
+            Text = "Симулятор";
             panel1.ResumeLayout(false);
-            panel1.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel1;
-        private Button StartStop;
-        private TextBox Location;
-        private ProgressBar progressBar1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private ListBox listlog;
+        private Label label1;
         private Button review;
+        private new TextBox Location;
+        private Button StartStop;
+        private TabPage tabPage2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,94 +1,221 @@
-using EasyModbus;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using Modbus.Device;
+using Modbus.Data;
+using System.Threading;
+using Microsoft.VisualBasic.Logging;
+
 
 namespace ModbasServer
 {
     public partial class Form1 : Form
     {
-        bool start_stop = false;
-        ModbusServer server;
-
+        private bool start_stop = true;
+        private static DataStore DataValue;
+        private static DataStore[] DataRangs;
+        private static ModbusSlave mb_tcp_server;
+        private static Thread ListenThred;
+        private static Thread GenerateData;
 
 
         public Form1()
         {
             InitializeComponent();
+            label1.Text = "";
+
+            DataValue = DataStoreFactory.CreateDefaultDataStore();
         }
 
+
+        /// <summary>
+        /// Конвертация и запись в регистры текста программы
+        /// </summary>
+        /// <param name="Text">Адрес на файл</param>
         private void transfer(string Text)
         {
-
-            ModbusServer.HoldingRegisters reg = server.holdingRegisters;
-            int count = 8001;
-            var rang = File.ReadAllLines(Text);
-            foreach (var line in rang)
+            var rangs = File.ReadAllLines(Text);
+            DataRangs = new DataStore[rangs.Length];
+            string line;
+            DataStore rang;
+            //текст ранга
+            for (int ind_Rang = 0; ind_Rang < rangs.Length; ind_Rang++)
             {
-                short temp = 0;
-                for (int i = 0; i < line.Length;)
+                int count = 8001+ind_Rang;
+                line = rangs[ind_Rang];
+                rang = DataStoreFactory.CreateDefaultDataStore();
+                ushort temp = 0;
+                for (int i = 0; i < 240;)
                 {
-                    if (line[i] != 0)
+                    if (line[i] != 0 && i < line.Length)
                     {
-                        temp = (short)line[i];
+                        temp = line[i];
                         i++;
                     }
                     if (i < line.Length)
                     {
-                        temp |= (short)((short)line[i] << 8);
+                        temp |= (ushort)(line[i] << 8);
                         i++;
-                        reg[count] = temp;
+                        rang.HoldingRegisters[count] = temp;
                         count++;
                     }
+                    if (i == line.Length) break;
                 }
-                //progressBar1.Value = 1;
+                DataRangs[ind_Rang] = rang;
             }
         }
 
+        /// <summary>
+        /// Открытие диологового окна
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void review_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.ShowDialog();
-            //MessageBox.Show(ofd.FileName); 
             Location.Text = ofd.FileName;
-            //progressBar1.Maximum = Location.Text.Length;
-        }
+        }   
 
+        /// <summary>
+        /// Запуск и остановка сервера
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void StartStop_Click(object sender, EventArgs e)
         {
-            if (Location.Text != "")
+            try
             {
                 switch (start_stop)
-                {
-                    case true:
+                    {
+                    case false:
                         {
-                            server.StopListening();
-                            server = null;
+                            mb_tcp_server.ModbusSlaveRequestReceived -= Mb_tcp_server_ModbusSlaveRequestReceived;
+                            mb_tcp_server.Dispose();
+
                             StartStop.BackColor = Color.Green;
                             StartStop.Text = "Старт";
                             start_stop = !start_stop;
+                            label1.Text = "";
+                            try
+                            {
+                                GenerateData.Abort(100);
+                                ListenThred.Abort(100);
+                            }
+                            catch
+                            {
+                                ListenThred.Join();
+                                listlog.Items.Add("Server stoped");
+                                ListenThred = null;
+                                GenerateData = null;
+                            }
+
                             break;
                         }
-                    case false:
+                    case true:
                         {
+                            mb_tcp_server = ModbusTcpSlave.CreateTcp(2, new TcpListener(new IPAddress(new byte[] { 0, 0, 0, 0 }), 502));
+                            mb_tcp_server.DataStore = DataValue;
+                            mb_tcp_server.ModbusSlaveRequestReceived += Mb_tcp_server_ModbusSlaveRequestReceived;
+
+                            start_stop = !start_stop;
                             StartStop.BackColor = Color.Red;
                             StartStop.Text = "Стоп";
-                            server = new ModbusServer();
-                            server.Listen();/*
-                            ModbusServer.HoldingRegisters reg = server.holdingRegisters;
-                            reg[1] = [4324, 234234, 223];*/
-                            start_stop = !start_stop;
-                            var trans = new Task(() =>
-                            {
-                                transfer(Location.Text);
-                            });
-                            trans.Start();
+                            label1.Text = "127.0.0.1:502";
+
+                            
+
+                            if(ListenThred == null)
+                                ListenThred = new Thread(() =>
+                                {
+                                    try
+                                    {
+                                        mb_tcp_server.ListenAsync();
+                                    }
+                                    catch (ThreadAbortException ex)
+                                    {
+                                        BeginInvoke(new MethodInvoker(() => { listlog.Items.Add("Thread(th_0) is aborted " + ex.ExceptionState); }));
+                                    }
+                                })
+                                {
+                                    IsBackground = true,
+                                };
+                            ListenThred.Name = "Listen";
+                            ListenThred.SetApartmentState(ApartmentState.STA);
+
+                            if(GenerateData == null)
+                                GenerateData = new Thread(() =>
+                                {
+                                    try
+                                    {
+                                        SetData();
+                                    }
+                                    catch (ThreadAbortException ex)
+                                    {
+                                        BeginInvoke(new MethodInvoker(() => { listlog.Items.Add("Thread(th_1) is aborted " + ex.ExceptionState); }));
+                                    }
+                                })
+                                {
+                                    IsBackground = true,
+                                };
+
+                            GenerateData.Name = "SetData";
+
+                            ListenThred.Start();
+                            GenerateData.Start();
+
+                            listlog.Items.Add("Server started");
+                            if (Location.Text != "") transfer(Location.Text);
                             break;
                         }
+                    }
+            }
+            catch(Exception ex) { MessageBox.Show(ListenThred.ThreadState.ToString()+" "+ex.Message); }
+        }
+
+        /// <summary>
+        /// Задача и обновление регистров
+        /// </summary>
+        public static void SetData()
+        {
+            int[] adres = new int[] { 1000, 600, 1200, 2000, 7200, 1300, 7000, 6800 };
+            Random random = new Random();
+            while (true)
+            {
+                Thread.Sleep(200);
+                foreach (int ad in adres)
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        DataValue.HoldingRegisters[ad + 1 + i] = (ushort)Math.Abs(random.Next(0, ushort.MaxValue));
+                    }
                 }
             }
-            else
+        }
+
+        /// <summary>
+        /// Формирование ответа, в зависиости от полученного зпроса
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Mb_tcp_server_ModbusSlaveRequestReceived(object sender, ModbusSlaveRequestEventArgs e)
+        {
+            //(e.Message.MessageFrame -> byte[])ind:0 - SlaveID; ind:1 - FunCod; ind:2,3 - adres; ind:4,5 - len
+            BeginInvoke(new MethodInvoker(() => 
+            { 
+                listlog.Items.Add(e.Message);
+                if (listlog.Items.Count > 14) listlog.Items.Remove(listlog.Items[0]);
+                listlog.SelectedIndex = listlog.Items.Count - 1;
+            }));
+
+            if (((e.Message.MessageFrame[2] * 256 + e.Message.MessageFrame[3]) /1000 == 8) && (e.Message.FunctionCode == 3))
             {
-                MessageBox.Show("Укажите файл!", "Ошибка!");
-                review_Click(sender, e);
+                if ((e.Message.MessageFrame[2] * 256 + e.Message.MessageFrame[3]) % 100 < DataRangs.Length && (e.Message.MessageFrame[2] * 256 + e.Message.MessageFrame[3]) % 100 >= 0)
+                    mb_tcp_server.DataStore = DataRangs[(e.Message.MessageFrame[2] * 256 + e.Message.MessageFrame[3]) % 100];
+                else
+                    mb_tcp_server.DataStore = DataValue;
             }
+            else if(DataRangs.Contains(mb_tcp_server.DataStore)) mb_tcp_server.DataStore = DataValue;
         }
     }
 }
